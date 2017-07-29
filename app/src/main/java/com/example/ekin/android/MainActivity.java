9627970eb7866f8d7967c3a0573f8ccc53fe9c17
Rabia_Ozkan <rabiaozkan01@gmail.com>
2017-07-29 15:26:31 +0300

    Add func is added

--- app/src/main/java/com/example/ekin/android/MainActivity.java
+++ app/src/main/java/com/example/ekin/android/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -25,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
         ALissues = new ArrayList<String>(); //initialize ALissues
         AAissues = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, ALissues);//initialize AAissues
         LWissues.setAdapter(AAissues);
-
     }
 
     public void addIssue(View v) {
